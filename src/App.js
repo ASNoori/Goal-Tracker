7fa13e5eb2d076f15d84ba94467990955e30c282
ASNoori <nooriameer12@gmail.com>
2024-07-09 13:33:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import GoalForm from "./Components/GoalForm";
+import Header from "./Components/Header";
+import Inprogress from "./Components/Inprogress";
+import Completed from "./Components/Completed";
 
 function App() {
+  const [goals, setGoals] = useState(
+    JSON.parse(localStorage.getItem("goals")) || []
+  );
+  const [editingGoal, setEditingGoal] = useState("");
+
+  // Update local storage whenever goals change
+  useEffect(() => {
+    localStorage.setItem("goals", JSON.stringify(goals));
+    console.log(goals);
+  }, [goals]);
+
+  const addGoal = (newGoal) => {
+    if (editingGoal) {
+      setGoals(
+        goals.map((goal) => (goal.id === editingGoal.id ? newGoal : goal))
+      );
+      setEditingGoal("");
+    } else {
+      setGoals((goals) => [...goals, newGoal]);
+    }
+  };
+  const editGoal = (goalEl) => {
+    setEditingGoal(goalEl);
+  };
+  const changeStatus = (id) => {
+    setGoals((goals) => {
+      return goals.map((goal) =>
+        goal.id === id ? { ...goal, completed: !goal.completed } : goal
+      );
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <GoalForm onAdd={addGoal} editingGoal={editingGoal} />
+      <div className="goal-list">
+        <Inprogress
+          goals={goals}
+          ongoalEdit={editGoal}
+          setGoals={setGoals}
+          changeStatus={changeStatus}
+        />
+        <Completed goals={goals} changeStatus={changeStatus} />
+      </div>
     </div>
   );
 }
